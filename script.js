d06089009fd56b1070ca9a947a442143008a1b3a
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2022-02-20 18:53:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ function calc(tippercent){
         var person=document.getElementById("people").value;
         console.log(tippercent);
         var tips=billamount*(tippercent/100);
+        var tipeach=tips/person;
         var totalbill=+billamount+tips;
 
         var billforeach=totalbill/person;
 
         var a=document.getElementById("tipamount");
-        a.innerHTML=tips.toFixed(2);
+        a.innerHTML=tipeach.toFixed(2);
 
         var b= document.getElementById("total");
         b.innerHTML=billforeach.toFixed(2);
